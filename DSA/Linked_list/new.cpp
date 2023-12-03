ef385eb5091c85435e77cc0e31b0a6127a970417
BijoySing <bijoysingh573@gmail.com>
2023-12-03 14:01:18 +0600

    organize folder structure

---
+++ DSA/Linked_list/new.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << endl
+#define int long long
+#define Int int
+#define ck(x) cout << #x << " = " << x << endl;
+int32_t main()
+{
+    int t = 1;
+    while (t--) {
+        for(int i = 0; i <100 ; i++) {
+            // Your code here
+            ck(i);
+        }
+    }
+}
