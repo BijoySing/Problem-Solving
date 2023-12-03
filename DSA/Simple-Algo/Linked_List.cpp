e3c98c68bf0192cd8a2defb78fd0df5d4787c8f1
BijoySing <bijoysingh573@gmail.com>
2023-12-03 13:56:55 +0600

    organize folder structure

---
+++ DSA/Simple-Algo/Linked_List.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << endl
+#define int long long
+#define Int int
+#define ck(x) cout << #x << " = " << x << endl;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+   
+    
+
+};
+
+int32_t main()
+{
+    int t = 1;
+    while (t--) {
+        Node a,b;
+        a.val=10;
+        b.val=20;
+        a.next=&b;
+        b.next=NULL;
+        cout<<a.val<<endl;
+    }
+}
