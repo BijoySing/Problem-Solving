864370154ebf2438401b49d7007c918a944cc700
BijoySing <bijoysingh573@gmail.com>
2023-12-03 11:04:56 +0600

    done

---
+++ DSA/Linked_List.cpp
@@ -0,0 +1,18 @@
+/**
+author: Bijoy Singh Rajput
+ "Nam Tho Sunahi Hoga"
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << endl
+#define int long long
+#define ii int
+#define ck(x) cout << #x << " = " << x << endl;
+int32_t main()
+{
+    // Your code here
+
+}
