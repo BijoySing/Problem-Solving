97e598cc23f8fa1f1b9c28f80c95710ab7d50e85
BijoySing <bijoysingh573@gmail.com>
2024-02-21 14:18:10 +0600

    bellmon ford algo

---
+++ DSA/Bellmon ford/Detect_Neg_cycle.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << endl
+#define ll long long
+#define Int int
+#define ck(x) cout << #x << " = " << x << endl;
+// Some code
+class Edge
+{
+public:
+    int u, v, c;
+    Edge(int u, int v, int c)
+    {
+        this->u = u;
+        this->v = v;
+        this->c = c;
+    }
+};
+const int N = 1e5 + 5;
+int dis[N];
+int32_t main()
+{
+    // Some code
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> EdgeList;
+    while (e--)
+    {
+        int u, v, c;
+        cin >> u >> v >> c;
+        EdgeList.push_back(Edge(u, v, c));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (Edge ed : EdgeList)
+        {
+            int u = ed.u;
+            int v = ed.v;
+            int c = ed.c;
+            if (dis[u] < INT_MAX and dis[u] + c < dis[v])
+            {
+                dis[v] = dis[u] + c;
+            }
+        }
+    }
+bool cycle=false;
+      for (int i = 0; i < n - 1; i++)
+    {
+        for (Edge ed : EdgeList)
+        {
+            int u = ed.u;
+            int v = ed.v;
+            int c = ed.c;
+            if (dis[u] < INT_MAX and dis[u] + c < dis[v])
+            {
+                cycle=true;
+            }
+        }
+    }
+   cycle?cout<<"Cycle Found!":cout<<"No Cycle";
+}
