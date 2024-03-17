e9d78d55e5a3660a6442225e378110e7a1408d3c
BijoySing <bijoysingh573@gmail.com>
2024-03-17 10:56:16 +0600

    day-1 complited

---
+++ practice/week-1/day-1/button.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << endl
+#define int long long
+#define Int int
+#define ck(x) cout << #x << " = " << x << endl;
+const int N = 1e5 + 5;
+int ok[N];
+
+void make()
+{
+     priority_queue<int, vector<int>, greater<int>> q; 
+       q.push(1);
+    ok[1] = 1;
+    while (!q.empty())
+    {
+        int a = q.top() * 2;
+        int b = q.top() + 3;
+        q.pop();
+        if(a < N and ok[a]==0){
+            q.push(a);
+            ok[a] = 1;
+        }
+        
+        if(b < N and ok[b]==0){
+            q.push(b);
+            ok[b] = 1;
+        }
+   }
+}
+
+int32_t main()
+{
+    
+    int t=1;
+    
+    while (t--)
+    {
+
+        int  a,b;
+        cin>>a>>b;
+        int ans= max(a+b,max(a+a-1,b+b-1));
+        cout<<ans<<endl;
+        }
+}
