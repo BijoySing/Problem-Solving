c21f0ee4cdfb64ff7ad14e7903c4c34213d77c7f
BijoySing <bijoysingh573@gmail.com>
2024-05-26 15:32:33 +0600

    day1

---
+++ practice/week-10/Day1/Good.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << "\n"
+#define int long long int
+#define ck(x) cout << #x << " = " << x << "\n"
+#define endl "\n"
+#define pb(x) push_back(x)
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        map<int, int> mp;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+        int ans = 0;
+        int val = (n) / 2;
+        // sort(mp.begin(), mp.end());
+        int f = 0;
+        for (auto x : mp)
+        {
+            if (x.second > val)
+            {
+                f = 1;
+                break;
+            }
+        }
+        if (f)
+        {
+            cout << "NO\n";
+        }
+        else
+        {
+            cout << "YES\n";
+        }
+    }
+}
