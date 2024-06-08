66052282e0506a528f05c412c95c8d024a6f7f26
BijoySing <bijoysingh573@gmail.com>
2024-06-08 23:13:45 +0600

    day1

---
+++ practice/week-12/Day1/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << "\n"
+#define int long long int
+#define ck(x) cout << #x << " = " << x << "\n"
+#define endl "\n"
+#define pb(x) push_back(x)
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        
+    }
+}
