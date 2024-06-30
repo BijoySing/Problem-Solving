9cc79ba012d8411529ba6698e665ee19ebd00140
BijoySing <bijoysingh573@gmail.com>
2024-06-30 13:56:04 +0600

    day1 completed

---
+++ practice/week-14/Day1/3SUM.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << "\n"
+#define int long long int
+#define ck(x) cout << #x << " = " << x << "\n"
+#define endl "\n"
+#define pb(x) push_back(x)
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        vector<int> v;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+
+            cin >> a[i];
+            int x=a[i]%10;
+            mp[x]++;
+            if(mp[x]<=3)
+            {
+                v.pb(a[i]);
+            }
+        }
+        sort(v.begin(), v.end());
+        int ans = 0;
+        int f=0;
+        for (int i = 0; i < v.size(); i++)
+        {
+            for (int j = i + 1; j < v.size(); j++)
+            {
+                for (int k = j + 1; k < v.size(); k++)
+                {
+                    if ((v[i] + v[j] + v[k]) % 10 == 3)
+                    {
+                        f=1;
+                        break;  
+                    }
+
+
+                }
+                if(f==1)
+                {
+                    break;
+                }
+            }
+            if(f==1)
+            {
+                break;
+            }
+        }
+        if(f==1)
+        {
+            yes;
+        }
+        else
+        {
+            no;
+        }
+    }
+}
