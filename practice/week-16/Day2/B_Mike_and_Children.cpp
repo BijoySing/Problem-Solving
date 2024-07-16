9434e7e0602ab726046dbfdba057a06920d9be12
bijoysing <bijoysingh573@gmail.com>
2024-07-16 19:43:03 +0600

    day2

---
+++ practice/week-16/Day2/B_Mike_and_Children.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define int long long int
+#define endl "\n"
+#define pb push_back
+#define pp(x) cout << x << endl
+#define ck(x) cout << #x << " = " << x << endl;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+
+            for (int j = i + 1; j < n; j++)
+            {
+                int x = a[i] + a[j];
+                // sum.pb(x);
+                mp[x]++;
+            }
+            
+        }
+
+
+        for (auto i : mp)
+        {
+            ans = max(ans, i.second);
+        }
+        pp(ans);
+    }
+
+}
