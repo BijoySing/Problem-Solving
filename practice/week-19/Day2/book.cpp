355d96b6bac57b916d7d01aabdd6023df43167d0
BijoySing <bijoysingh573@gmail.com>
2024-09-04 23:16:00 +0600

    day3

---
+++ practice/week-19/Day2/book.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << "\n"
+#define int long long int
+#define ck(x) cout << #x << " = " << x << "\n"
+#define endl "\n"
+#define pb(x) push_back(x)
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int s=a[n-1];
+        int x=(n+1)/2;
+        sort(a,a+n-1);
+        s+=a[n-2];
+
+        cout<<s<<endl;
+    }
+}
---
+++ practice/week-19/Day3/Bakery.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pp(x) cout << x << "\n"
+#define int long long int
+#define ck(x) cout << #x << " = " << x << "\n"
+#define endl "\n"
+#define pb(x) push_back(x)
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n;
+        cin >> a >> b;
+        ll ans = n * a;
+        b = max(0ll, b - a);
+        if (b <= n)
+            ans += (b * (b + 1) / 2);
+        else
+            ans += (n * (b + b - n + 1) / 2);
+        cout << ans << endl;
+    }
+}
